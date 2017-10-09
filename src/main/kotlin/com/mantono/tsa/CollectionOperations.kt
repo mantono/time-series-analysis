6ce861239686e90d8c954fd7a6fc40641e74619c
Anton Österberg <anton.osterberg@gmail.com>
2017-10-09 22:55:26 +0200

    Change a sequence of Pairs of Instant and Number to a sequence of DataPoints.

--- src/main/kotlin/com/mantono/tsa/CollectionOperations.kt
+++ src/main/kotlin/com/mantono/tsa/CollectionOperations.kt
@@ -1,5 +1,7 @@
 package com.mantono.tsa
 
+import java.time.Instant
+
 fun <T: Number> Iterable<T>.sum(): Double = sumByDouble { it.toDouble() }
 fun <T: Number> Iterable<T>.average(): Double = asSequence().map { it.toDouble() }.average()
 
@@ -25,3 +27,7 @@ fun <T: Number> Iterable<DataPoint<T>>.sortedSubList(include: IntRange): List<Da
 fun <T> Iterable<T>.subList(include: IntRange): List<T> = asSequence().subList(include)
 fun <T> Sequence<T>.subList(include: IntRange): List<T> = filterIndexed { i, _ -> i in include }.toList()
 fun <T> Sequence<T>.subSequence(include: IntRange): Sequence<T> = filterIndexed { i, _ -> i in include }
+
+fun <T: Number> Sequence<Pair<Instant, T>>.asDataPoints() = this.map { DoubleDataPoint(it.first, it.second.toDouble()) }
+
+data class DoubleDataPoint(override val timestamp: Instant, override val value: Double): DataPoint<Double>
